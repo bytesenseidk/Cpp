dacbe9bc821a2e14cc5cacd5be5aa0e985474507
Lars Rosenkilde <lars.rosenkilde95@gmail.com>
2021-12-13 23:20:44 +0100

    Added FunctionOverload.cpp

---
+++ FunctionOverload.cpp
@@ -0,0 +1,12 @@
+class Addition {
+public:
+    static int add(int a, int b) {
+        return a + b;
+    }
+    static float add(float a, float b) {
+        return a + b;
+    }
+    static double add(double a, double b) {
+        return a + b;
+    }
+};
--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include "Fibonacci.cpp"
+#include "FunctionOverload.cpp"
 
 using namespace std;
 
 int main() {
     signed int num0 = 17;
     signed int num1 = 22;
+    float num2 = 1.27;
+    double num3 = 1.234567;
 
     cout << "Fibonacci number at " << num0 << "'th place: " << FibonacciClass::basicFibonacci(num0) << endl;
     cout << "Fibonacci number at " << num1 << "'th place: " << FibonacciClass::recursiveFibonacci(num1) << endl;
